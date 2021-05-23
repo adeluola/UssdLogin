7d4c24a6b55d8a880234e8d4e762a7504457206c
charles-annan <bimbodelu@gmail.com>
2021-05-23 21:24:28 +0100

    navbar and jumbotron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule,
